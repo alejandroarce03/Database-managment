ecb27557b7d74b371539f5aec02d4b9f34f6c9c5
Ricardo D. Cuero <47835668+merolemay@users.noreply.github.com>
2020-11-06 22:08:40 -0500

    Test Clases  added to java

---
+++ Test/Test/BSTAvlTest.java
@@ -0,0 +1,34 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BSTAvlTest {
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCheckAVLBalance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDepth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSearch() {
+		fail("Not yet implemented");
+	}
+
+}
